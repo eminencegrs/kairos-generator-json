1a16f9cb9a6497403b45d25f3161452ddc21ce01
Andrii Dariienko <andrii.dariienko@outlook.com>
2019-11-01 18:20:32 +0200

    Refactoring | Improve unit tests.

--- tests/Kairos.Generator.Randomizer.Tests/RandomIntTests.fs
+++ tests/Kairos.Generator.Randomizer.Tests/RandomIntTests.fs
@@ -1,6 +1,7 @@
 ﻿namespace Kairos.Generator.Randomizer.Tests
 
 open Xunit
+open FluentAssertions
 open Kairos.Generator.Randomizer
 
 module RandomIntTests =
@@ -8,20 +9,18 @@ module RandomIntTests =
     [<Fact>]
     let ``Getting_Random_Int_Beetween_minus10_and_10`` () =
         let actualResult = RandomInt.getAnyFromTo -10 10
-        Assert.NotNull(actualResult)
-        Assert.IsAssignableFrom<int>(actualResult) |> ignore
-        Assert.InRange<int>(actualResult, -10, 10)
+        actualResult.Should().BeOfType(typeof<int>, "because it's expected to be 'int'") |> ignore
+        actualResult.Should().BeInRange(-10, 10, "because it's expected to be beetween '-10' and '10'") |> ignore
 
     [<Fact>]
     let ``Getting_Random_Positive_Int_Beetween_0_and_10`` () =
         let actualResult = RandomInt.getAnyFromTo 0 10
-        Assert.NotNull(actualResult)
-        Assert.IsAssignableFrom<int>(actualResult) |> ignore
-        Assert.InRange<int>(actualResult, 0, 10)
+        actualResult.Should().BeOfType(typeof<int>, "because it's expected to be 'int'") |> ignore
+        actualResult.Should().BeInRange(0, 10, "because it's expected to be beetween '0' and '10'") |> ignore
 
     [<Fact>]
     let ``Getting_Random_Negative_Int_Beetween_minus100_and_minus1`` () =
         let actualResult = RandomInt.getAnyFromTo -100 -1
-        Assert.NotNull(actualResult)
-        Assert.IsAssignableFrom<int>(actualResult) |> ignore
-        Assert.InRange<int>(actualResult, -100, -1)
+        actualResult.Should().BeOfType(typeof<int>, "because it's expected to be 'int'") |> ignore
+        actualResult.Should().BeInRange(-100, -1, "because it's expected to be beetween '-100' and '-1'") |> ignore
+
